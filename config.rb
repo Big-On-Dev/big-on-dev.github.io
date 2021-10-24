faf14f94cbdc4b0561f23b53e168c1f407d6acdd
Weiyuan Liu <weiyuan.lane@gmail.com>
2021-10-24 12:33:04 +0800

    [Feature] Add shortened links

--- config.rb
+++ config.rb
@@ -79,3 +79,22 @@ configure :build do
 end
 
 activate :directory_indexes
+
+events_str = File.read('data/events.json')
+events = JSON.parse(events_str)
+
+combined_events = events['upcoming'] + events['past']
+combined_events.each do |event|
+  if event.key?('shortenedLink')
+    shortened_link = event['shortenedLink']
+    if !shortened_link.key?('placeholder') || !shortened_link['placeholder'].instance_of?(String) || shortened_link['placeholder'] == ''
+      raise Exception.new('Placeholder is missing')
+    end
+
+    if !shortened_link.key?('destination') || !shortened_link['destination'].instance_of?(String) || shortened_link['destination'] == ''
+      raise Exception.new('Destination is missing')
+    end
+
+    proxy "/-/#{shortened_link['placeholder']}/index.html", '/rd/template.html', :locals => { :rd => event }, :ignore => true
+  end
+end
